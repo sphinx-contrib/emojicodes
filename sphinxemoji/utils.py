14d709557b3d19fff8e6f495c010961a2dc30fd3
Miguel Sánchez de León Peque <peque@neosit.es>
2019-06-14 17:30:52 +0200

    Retrieve emoji codes from Gemojione

--- sphinxemoji/utils.py
+++ sphinxemoji/utils.py
@@ -3,12 +3,13 @@ from urllib.request import urlopen
 
 
 def update_codes():
-    remote = 'https://gitlab.com/gitlab-org/gitlab-ce/raw/master'
-    data = json.load(urlopen(remote + '/fixtures/emojis/index.json'))
-    codes = {emoji['shortname']: emoji['moji'] for emoji in data.values()}
-    aliases = json.load(urlopen(remote + '/fixtures/emojis/aliases.json'))
-    for alias, original in aliases.items():
-        codes[':%s:' % alias] = codes[':%s:' % original]
+    remote = 'https://raw.githubusercontent.com/bonusly/gemojione/master'
+    data = json.load(urlopen(remote + '/config/index.json'))
+    codes = {}
+    for emoji in data.values():
+        codes[emoji['shortname']] = emoji['moji']
+        for alias in emoji['aliases']:
+            codes[alias] = emoji['moji']
     with open('sphinxemoji/codes.json', 'w') as output:
         json.dump(codes, output, sort_keys=True, indent=4, ensure_ascii=False)
         output.write('\n')
