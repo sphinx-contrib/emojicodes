338b2a3808e095bd545b7df25fa962ddade2fed3
Miguel Sánchez de León Peque <peque@neosit.es>
2020-04-04 21:17:29 +0200

    Do not remove old emoji codes from the list

--- sphinxemoji/utils.py
+++ sphinxemoji/utils.py
@@ -2,7 +2,7 @@ import json
 from urllib.request import urlopen
 
 
-def update_codes():
+def get_gemojione():
     remote = 'https://raw.githubusercontent.com/bonusly/gemojione/master'
     data = json.load(urlopen(remote + '/config/index.json'))
     codes = {}
@@ -10,6 +10,16 @@ def update_codes():
         codes[emoji['shortname']] = emoji['moji']
         for alias in emoji['aliases']:
             codes[alias] = emoji['moji']
+    return codes
+
+
+def update_codes():
+    with open('sphinxemoji/codes.json') as current:
+        codes = json.load(current)
+    for getter in [
+        get_gemojione,
+    ]:
+        codes.update(getter())
     with open('sphinxemoji/codes.json', 'w') as output:
         json.dump(codes, output, sort_keys=True, indent=4, ensure_ascii=False)
         output.write('\n')
